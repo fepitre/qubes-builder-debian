7f884f89de80a23676d775253a0d5f635090311e
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2023-05-02 14:10:23 +0200

    Rework flavor denomitations to match Fedora ones too
    - Several fixes too.

--- template_ubuntu/flash/02_install_groups_packages_installed.sh
+++ template_ubuntu/flash/02_install_groups_packages_installed.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 # vim: set ts=4 sw=4 sts=4 et :
 
-source "${SCRIPTSDIR}/vars.sh"
-source "${SCRIPTSDIR}/distribution.sh"
+# shellcheck source=template_ubuntu/vars.sh
+source "${TEMPLATE_CONTENT_DIR}/vars.sh"
+# shellcheck source=template_ubuntu/distribution.sh
+source "${TEMPLATE_CONTENT_DIR}/distribution.sh"
 
 #### '----------------------------------------------------------------------
 info ' Installing flash plugin'
